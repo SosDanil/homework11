717bee4cc51f5abdd9d91007ef70c8e54d7e7bc8
Danil Klimenko <sos.danil@yandex.ru>
2023-12-12 00:16:54 +0500

    add nwe func and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def big_letters(word):
+    """делает все буквы заглавными"""
     return word.upper()
+
+
+def title_text(text):
+    """делают первую букву каждого слова заглавной"""
+    return text.title()
